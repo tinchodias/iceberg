e1b33da9d8d7614e6ff43d4c9f28d93854b2ccba
Martín Dias <tinchodias@gmail.com>
2020-06-03 19:50:59 -0400

    Update loading scripts

--- scripts/testUpdateIceberg.sh
+++ scripts/testUpdateIceberg.sh
@@ -2,5 +2,7 @@
 set -x
 set -e
 
-wget -O - get.pharo.org/70+vm | bash
-./pharo Pharo.image st --save --quit scripts/updateIceberg.st 
+wget -O - get.pharo.org/80+vm | bash
+./pharo Pharo.image st --save --quit scripts/preLoading.st
+./pharo Pharo.image st --save --quit scripts/load.st
+./pharo Pharo.image st --save --quit scripts/postLoading.st
