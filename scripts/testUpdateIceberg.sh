95409c9517db37e840a4547c7e1e726e6a17fb90
Martín Dias <tinchodias@gmail.com>
2020-06-04 17:23:12 -0400

    Pass on update script

--- scripts/testUpdateIceberg.sh
+++ scripts/testUpdateIceberg.sh
@@ -2,7 +2,9 @@
 set -x
 set -e
 
-wget -O - get.pharo.org/80+vm | bash
-./pharo Pharo.image st --save --quit scripts/preLoading.st
-./pharo Pharo.image st --save --quit scripts/load.st
-./pharo Pharo.image st --save --quit scripts/postLoading.st
+REPO_ROOT="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+
+wget -O - get.pharo.org/90+vm | bash
+./pharo Pharo.image st --save --quit "$REPO_ROOT/scripts/preLoading.st"
+./pharo Pharo.image metacello install "tonel://$REPO_ROOT" BaselineOfIceberg --groups=development
+./pharo Pharo.image st --save --quit "$REPO_ROOT/scripts/postLoading.st"
