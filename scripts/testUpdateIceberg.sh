aabf4346a839b87bad82c9bfe8226975afcf5d6d
Martín Dias <tinchodias@gmail.com>
2020-10-13 14:58:39 -0300

    wget --> curl

--- scripts/testUpdateIceberg.sh
+++ scripts/testUpdateIceberg.sh
@@ -12,7 +12,7 @@ fi
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 REPO_DIR="$(dirname "$SCRIPTS_DIR")"
 
-wget -O - get.pharo.org/64/90+vm | bash
+curl https://get.pharo.org/64/90+vm | bash
 ./pharo Pharo.image st --save --quit "$REPO_DIR/scripts/preLoading.st"
 ./pharo Pharo.image metacello install "tonel://$REPO_DIR" BaselineOfIceberg --groups=$BASELINE_GROUPS
 ./pharo Pharo.image st --save --quit "$REPO_DIR/scripts/postLoading.st"
