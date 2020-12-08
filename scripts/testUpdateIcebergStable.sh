cc2f256864b0f503953508d2b5478aba5f84184d
Martín Dias <tinchodias@gmail.com>
2020-12-08 18:59:14 -0300

    Fix script to also work on MINGWIN

--- scripts/testUpdateIcebergStable.sh
+++ scripts/testUpdateIcebergStable.sh
@@ -9,11 +9,21 @@ else
     BASELINE_GROUPS=default
 fi
 
-SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+case "$(uname -s)" in
+
+   MINGW*)
+     SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -W )"
+     ;;
+
+   *)
+     SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+     ;;
+esac
+
 REPO_DIR="$(dirname "$SCRIPTS_DIR")"
 
 curl https://get.pharo.org/64/vm90 | bash
-wget http://files.pharo.org/image/90/Pharo9.0-SNAPSHOT.build.832.sha.de1f37b.arch.64bit.zip
+curl -O http://files.pharo.org/image/90/Pharo9.0-SNAPSHOT.build.832.sha.de1f37b.arch.64bit.zip
 unzip Pharo9.0-SNAPSHOT.build.832.sha.de1f37b.arch.64bit.zip
 mv Pharo9.0-SNAPSHOT-64bit-de1f37b.image Pharo.image
 mv Pharo9.0-SNAPSHOT-64bit-de1f37b.changes Pharo.changes
